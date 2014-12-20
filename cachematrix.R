00a173d717c296d397c75e1ef365502cb1649e4d
Mrinal Govind <mrinal.govind@gmail.com>
2014-12-20 11:58:11 +0530

    Week 2 prog assignment: changes comments for better understanding

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
-## makeCacheMatrix() creates a special "matrix" object that stores as attributes its own value and its inverse.
-## Just after creation the special "matrix" object does not stores its inverse value and thus cacheSolve() fucntion must be called to get the inverse.
-## cacheSolve() called for the first time for a special "matrix" object computes the inverse of the special "matrix".
+## makeCacheMatrix() creates a special "matrix" object that stores, as attributes, its own value and its inverse.
+## Just after creation, the special "matrix" object does not stores its inverse value. 
+## Thus cacheSolve() fucntion must be called to get the inverse.
+## cacheSolve() called for the first time for a special "matrix" object, computes the inverse of the special "matrix".
 ## Upon subsequent calls of cacheSolve() for the same special "matrix" object (provided the underlying matrix value has not been modified)
 ## returns the inverse value from Cache without doing any computations for inverse.  
 
 
-## This function creates a special "matrix" object that stores as attributes its own value and its inverse.
+## This function creates a special "matrix" object that stores, as attributes, its own value and its inverse.
 ## The special "matrix" object is of type 'list', which exposes functions to access and mutate its attributes.
 
 makeCacheMatrix <- function(originalMatrix = matrix()) {  # variable 'originalMatrix' stores the actual matrix object with values
@@ -13,15 +14,16 @@ makeCacheMatrix <- function(originalMatrix = matrix()) {  # variable 'originalMa
     inverseOfMatrix <- NULL  # variable 'inverseOfMatrix' stores the inverse of matrix stored by 'originalMatrix' attribute
                              # at initial call of the method its always set to NULL.
     
-    setMatrix <- function(newMatrix) {  # allows changing of the matirx stored in attribute 'originalMatrix'
+    setMatrix <- function(newMatrix) {  # mutator function for 'originalMatrix' variable
         
         originalMatrix <<- newMatrix  # updates the 'originalMatrix' attribute with new matrix.
         
-        inverseOfMatrix <<- NULL  # resets the 'inverseOfMatrix' to null to take care of scenario when there is any change in the 'originalMatrix' attribute.
+        inverseOfMatrix <<- NULL  # resets the 'inverseOfMatrix' to null.
+                                  # forces recomputation of inverse when there is any change in the 'originalMatrix' attribute.
         
     }
     
-    getMatrix <- function() {  # returns the matrix refferenced by 'originalMatrix' attribute.
+    getMatrix <- function() {  # accessor function for 'originalMatrix', returns the matrix refferenced by 'originalMatrix' attribute.
         originalMatrix
     }
     
@@ -38,7 +40,7 @@ makeCacheMatrix <- function(originalMatrix = matrix()) {  # variable 'originalMa
     list(setMatrix = setMatrix, getMatrix = getMatrix,
          setInverseOfMatrix = setInverseOfMatrix,
          getInverseOfMatrix = getInverseOfMatrix)  # sets refferences to the accessor and mutator functions.
-
+    
 }
 
 
@@ -51,13 +53,13 @@ cacheSolve <- function(specilMatrixObject, ...) {
     cachedInverseOfMatrix <- specilMatrixObject$getInverseOfMatrix()  # reads the inverse of the sepcial "matrix" object. 
     
     if( ! is.null(cachedInverseOfMatrix)){  # the 'if' block returns the 'cachedInverseOfMatrix' if its not null in the special "matrix" object
-                                                    
+        
         message("returning value from cache")
         return(cachedInverseOfMatrix)  # returns the cached inverse of matrix and halts further processing of the function.
     }
     
     originalMatrix <- specilMatrixObject$getMatrix()  # gets the original underlying matix
-     
+    
     calculatedInverseOfMatrix <- solve(originalMatrix)  # computes the inverse of a matrix
     
     specilMatrixObject$setInverseOfMatrix(calculatedInverseOfMatrix)  # sets back the computed inverse of the matrix to the special "matrix" object
